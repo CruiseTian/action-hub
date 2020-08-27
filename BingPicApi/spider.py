1e6da37bbd24079460bcf9e14685fbf526e82993
CruiseTian <979376748@qq.com>
2020-08-27 14:50:38 +0800

    fix some bugs

--- BingPicApi/spider.py
+++ BingPicApi/spider.py
@@ -7,7 +7,7 @@ def GetPic():
     json_data = json.loads(api.text)
     pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
     start_date = json_data['images'][0]['startdate']
-    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+    open(r'./BingPicApi/json/{0}.json'.format(start_date), 'wb').write(api.content)
     print('Create Json Success!')
     pic = get(pic_url, stream=True)
     if(pic.status_code == 200):
