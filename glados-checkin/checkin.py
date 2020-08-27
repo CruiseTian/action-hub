d36ceedd2ded452c73265b275852c32c1f01bc38
CruiseTian <979376748@qq.com>
2020-08-27 23:48:11 +0800

    fix little bug

--- glados-checkin/checkin.py
+++ glados-checkin/checkin.py
@@ -1,7 +1,7 @@
 import requests,json,os
 
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = ‘on'
+sever = 'on'
 # 填写server酱sckey,不开启server酱则不用填
 sckey = os.environ["SCKEY"]
 #'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
