bd57a10c4d896e357a8e0ffe6fc927f1dc92a488
CruiseTian <979376748@qq.com>
2021-09-27 10:56:23 +0800

    update

--- AC-EUserv/main.py
+++ AC-EUserv/main.py
@@ -1,16 +1,37 @@
 import os
+import re
 import json
 import time
 import requests
 from bs4 import BeautifulSoup
 
-USERNAME = os.environ["USERNAME"]
-PASSWORD = os.environ["PASSWORD"]
-TOKEN = os.environ.get("TOKEN", "")
+# 多个账户请使用空格隔开
+USERNAME = os.environ["USERNAME"]  # 用户名或邮箱
+PASSWORD = os.environ["PASSWORD"]  # 密码
+
+# Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
+TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')  # 通过 @BotFather 申请获得，示例：1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
+TG_USER_ID = os.environ["TG_USER_ID"] # 用户、群组或频道 ID，示例：129xxx206
+TG_API_HOST = 'api.telegram.org'# 自建 API 反代地址，供网络环境无法访问时使用，网络正常则保持默认
+
+# Server酱 http://sc.ftqq.com/?c=code
+SCKEY = ''  # 这里填Server酱的key，无需推送可不填 示例: SCU646xxxxxxxxdacd6a5dc3f6
+
+# sre24 https://sre24.com/
+SRE24_TOKEN = ''  # 填sre24的token
+
 PROXIES = {
-    "http": "http://127.0.0.1:10809",
-    "https": "http://127.0.0.1:10809"
+    "http": "http://127.0.0.1:10808",
+    "https": "http://127.0.0.1:10808"
 }
+
+desp = ''  # 空值
+
+
+def log(info: str):
+    print(info)
+    global desp
+    desp = desp + info + '\n\n'
 
 
 def login(username: str, password: str) -> (str, requests.session):
@@ -19,21 +40,28 @@ def login(username: str, password: str) -> (str, requests.session):
                       "Chrome/83.0.4103.116 Safari/537.36",
         "origin": "https://www.euserv.com"
     }
+    url = "https://support.euserv.com/index.iphp"
+    session = requests.Session()
+
+    sess = session.get(url, headers=headers)
+    sess_id = re.findall("PHPSESSID=(\\w{10,100});", str(sess.headers))[0]
+    # 访问png
+    png_url = "https://support.euserv.com/pic/logo_small.png"
+    session.get(png_url, headers=headers)
+
     login_data = {
         "email": username,
         "password": password,
         "form_selected_language": "en",
         "Submit": "Login",
-        "subaction": "login"
+        "subaction": "login",
+        "sess_id": sess_id
     }
-    url = "https://support.euserv.com/index.iphp"
-    session = requests.Session()
     f = session.post(url, headers=headers, data=login_data)
     f.raise_for_status()
-    if f.text.find('Hello') == -1:
+
+    if f.text.find('Hello') == -1 and f.text.find('Confirm or change your customer data here') == -1:
         return '-1', session
-    # print(f.request.url)
-    sess_id = f.request.url[f.request.url.index('=') + 1:len(f.request.url)]
     return sess_id, session
 
 
@@ -98,55 +126,87 @@ def renew(sess_id: str, session: requests.session, password: str, order_id: str)
 
 
 def check(sess_id: str, session: requests.session):
-    print("Checking.......")
+    print("反馈中.......")
     d = get_servers(sess_id, session)
     flag = True
     for key, val in d.items():
         if val:
             flag = False
-            print("ServerID: %s Renew Failed!" % key)
-            TOKEN and notify_user(token=TOKEN, msg="ServerID: %s Renew Failed!" % key)
+            log("ServerID: %s 失败！德鸡吐血倒地，请自查原因!" % key)
+
     if flag:
-        print("ALL Work Done! Enjoy")
+        log("一切正常，德鸡在向你微笑！(≧▽≦)")
 
 
-def notify_user(token: str, msg: str):
-    rs = requests.post(url="https://sre24.com/api/v1/push", json=dict(token=token, msg=msg)).json()
-    assert int(rs["code"] / 100) == 2, rs
+# Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
+def telegram():
+    data = (
+        ('chat_id', TG_USER_ID),
+        ('text', 'EUserv续期日志\n\n' + desp)
+    )
+    response = requests.post('https://' + TG_API_HOST + '/bot' + TG_BOT_TOKEN + '/sendMessage', data=data)
+    if response.status_code != 200:
+        print('Telegram Bot 推送失败')
+    else:
+        print('Telegram Bot 推送成功')
+
+
+# Server酱 http://sc.ftqq.com/?c=code
+def server_chan():
+    data = (
+        ('text', 'EUserv续期日志'),
+        ('desp', desp)
+    )
+    response = requests.post('https://sc.ftqq.com/' + SCKEY + '.send', data=data)
+    if response.status_code != 200:
+        print('Server酱 推送失败')
+    else:
+        print('Server酱 推送成功')
+
+
+def sre24():
+    msg = 'EUserv续期日志\n\n' + desp
+    url = 'https://push.jwks123.cn/to/'
+    rs = requests.post(url, json=dict(token=SRE24_TOKEN, msg=msg)).json()
+    if int(rs["code"] / 100) != 2:
+        print('sre24 推送失败')
+    else:
+        print('sre24 推送成功')
 
 
 if __name__ == "__main__":
     if not USERNAME or not PASSWORD:
-        print("你没有添加任何账户")
-        TOKEN and notify_user(token=TOKEN, msg="你没有添加任何账户")
+        log("你没有添加任何账户")
         exit(1)
     user_list = USERNAME.strip().split()
     passwd_list = PASSWORD.strip().split()
     if len(user_list) != len(passwd_list):
-        print("The number of usernames and passwords do not match!")
-        TOKEN and notify_user(token=TOKEN, msg="The number of usernames and passwords do not match!")
+        log("The number of usernames and passwords do not match!")
         exit(1)
     for i in range(len(user_list)):
         print('*' * 30)
-        print("正在续费第 %d 个账号" % (i + 1))
+        log("正在续费第 %d 个账号" % (i + 1))
         sessid, s = login(user_list[i], passwd_list[i])
         if sessid == '-1':
-            print("第 %d 个账号登陆失败，请检查登录信息" % (i + 1))
-            TOKEN and notify_user(token=TOKEN, msg="第 %d 个账号登陆失败，请检查登录信息" % (i + 1))
+            log("第 %d 个账号登陆失败，请检查登录信息" % (i + 1))
+
             continue
         SERVERS = get_servers(sessid, s)
-        print("检测到第 {} 个账号有 {} 台VPS，正在尝试续期".format(i + 1, len(SERVERS)))
+        log("检测到第 {} 个账号有 {} 台VPS，正在尝试续期".format(i + 1, len(SERVERS)))
         for k, v in SERVERS.items():
             if v:
                 if not renew(sessid, s, passwd_list[i], k):
-                    print("ServerID: %s Renew Error!" % k)
-                    TOKEN and notify_user(token=TOKEN, msg="ServerID: %s Renew Error!" % k)
+                    log("ServerID: %s 运行失败！德鸡吐血倒地，请自查原因!" % k)
                 else:
-                    print("ServerID: %s has been successfully renewed!" % k)
-                    TOKEN and notify_user(token=TOKEN, msg="ServerID: %s has been successfully renewed!" % k)
+                    log("ServerID: %s 运行完毕！德鸡已到续期时间点，成功续期!" % k)
             else:
-                print("ServerID: %s does not need to be renewed" % k)
+                log("ServerID: %s 运行完毕！德鸡未到续期时间点，后会有期！" % k)
         time.sleep(15)
         check(sessid, s)
         time.sleep(5)
+
+    TG_BOT_TOKEN and TG_USER_ID and TG_API_HOST and telegram()
+    SCKEY and server_chan()
+    SRE24_TOKEN and sre24()
+
     print('*' * 30)
